a9bd7cf2ade1e68d7b1f579e785e4c8695ed8da1
victorazangu <victorsazangu@gmail.com>
2022-12-20 20:35:30 +0300

    merge changes

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import getResponseFromAPI from './0-promise.js';
-=======
-import getResponseFromAPI from "./0-promise";
->>>>>>> 93fc0c9af193db6831461ae277a09d745992da76
+import getResponseFromAPI from './0-promise';
 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
