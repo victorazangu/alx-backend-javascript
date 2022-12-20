548ef6fc4a4c566cb20caf3ba0576cfaa28d2f4c
victorazangu <victorsazangu@gmail.com>
2022-12-20 22:08:23 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((results) => {
+    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
