5855927212fdc9d7f4a59125339b857b8abf9157
victorazangu <victorsazangu@gmail.com>
2022-12-22 17:14:14 +0300

    corrections

--- 0x02-ES6_classes/10-main.js
+++ 0x02-ES6_classes/10-main.js
@@ -11,4 +11,4 @@ console.log(tc1 instanceof TestCar);
 console.log(tc2);
 console.log(tc2 instanceof TestCar);
 
-console.log(tc1 == tc2);
+console.log(tc1 === tc2);
--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,90 +1,50 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!Array.isArray(students) || !students.every((e) => typeof e === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
 
-// export default class HolbertonCourse {
-//   constructor(name, length, students) {
-//     this.name = name;
-//     this.length = length;
-//     this.students = students;
-//   }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-//   get name() {
-//     return this._name;
-//   }
+  // getters
+  get name() {
+    return this._name;
+  }
 
-//   set name(val) {
-//     if (typeof val !== 'string') { throw new TypeError('Name must be a string'); }
-//     this._name = val;
-//   }
+  get length() {
+    return this._length;
+  }
 
-//   get length() {
-//     return this._length;
-//   }
+  get students() {
+    return this._students;
+  }
 
-//   set length(val) {
-//     if (typeof val !== 'number') { throw new TypeError('Length must be a number'); }
-//     this._length = val;
-//   }
+  // setters
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
 
-//   get students() {
-//     return this._students;
-//   }
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
 
-//   set students(val) {
-//     if (!Array.isArray(val)) { throw new TypeError('Students must be an array'); }
-//     // eslint-disable-next-line no-plusplus
-//     for (let i = 0; i < val.length; i++) {
-//       if (typeof val[i] !== 'string') { throw new TypeError('Students must be an array of strings'); }
-//     }
-//     this._students = val;
-//   }
-// }
-
-export default class HolbertonCourse{
-    constructor(name, length, students) {
-        if (typeof name !== 'string') {
-          throw new TypeError('Name must be a string');
-        } else if (typeof length !== 'number') {
-          throw new TypeError('Length must be a number');
-        } else if (!Array.isArray(students) || !students.every((e) => typeof e === 'string')) {
-          throw new TypeError('Students must be an array of strings');
-        }
-    
-        this._name = name;
-        this._length = length;
-        this._students = students;
-      }
-
-      //getters
-      get name() {
-        return this._name;
-      }
-    
-      get length() {
-        return this._length;
-      }
-    
-      get students() {
-        return this._students;
-      }
-    //setters
-    set name(name) {
-        if (typeof name !== 'string') {
-          throw new TypeError('Name must be a string');
-        }
-        this._name = name;
-      }
-    
-      set length(length) {
-        if (typeof length !== 'number') {
-          throw new TypeError('Length must be a number');
-        }
-        this._length = length;
-      }
-    
-      set students(students) {
-        if (!Array.isArray(students) || !students.every((e) => typeof e === 'string')) {
-          throw new TypeError('Students must be an array of strings');
-        }
-        this._students = students;
-      }
+  set students(students) {
+    if (!Array.isArray(students) || !students.every((e) => typeof e === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
 }
-
