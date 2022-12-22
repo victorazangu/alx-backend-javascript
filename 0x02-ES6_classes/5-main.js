7c0a6710e3bf078b49224e2ed965a5be99f62a00
victorazangu <victorsazangu@gmail.com>
2022-12-22 17:32:54 +0300

    const

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -6,7 +6,7 @@ console.log(b);
 class TestBuilding extends Building {}
 
 try {
-    var test = new TestBuilding(200);
+    const test = new TestBuilding(200);
 } catch (err) {
   console.log(err);
 }
